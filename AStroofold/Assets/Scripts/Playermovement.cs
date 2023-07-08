9050e188ea02d9d915d9330603c846318c80e58d
vinicius <viniciusmartins.12@hotmail.com>
2023-07-08 11:18:57 -0300

    Criação blocagem
    Ajuste na blocagem da introdução e do primeiro Level.

--- AStroofold/Assets/Scripts/Playermovement.cs
+++ AStroofold/Assets/Scripts/Playermovement.cs
@@ -1,33 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Playermovement : MonoBehaviour
 {
     Rigidbody rb;
-    public float forceMovement;
-    public int velocity;
+    public float forwardSpeed = 10f;     // Velocidade de movimento para frente
+    public float sideSpeed = 5f;         // Velocidade de movimento lateral
+    public float smoothness = 5f;        // Suavidade do movimento lateral
 
-    private void Start()
+    private float targetPosition = 0f;   // Posição lateral alvo
+    private float currentVelocity = 0f;  // Velocidade atual do movimento lateral
+
+
+    //Controle UI
+    public Image painelGameOver;
+
+    private void Update()
     {
-        rb = GetComponent<Rigidbody>();
+        // Movimento para frente constante
+        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+
+        // Movimento lateral suave
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        targetPosition += moveHorizontal * sideSpeed * Time.deltaTime;
+        float newPosition = Mathf.SmoothDamp(transform.position.x, targetPosition, ref currentVelocity, smoothness);
+        transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
     }
 
-    void FixedUpdate()
+    void OnTriggerEnter(Collider other)
     {
-        //força de impulso para levar a nave para FRENTE;
-        rb.AddForce(0, 0, velocity * Time.fixedDeltaTime);
-
-
-        //força de impulso para levar a nave para os LADOS;
-        if (Input.GetKey ("a") == true)
+        if (other.CompareTag("Asteroid"))
         {
-            rb.AddForce(-forceMovement * Time.fixedDeltaTime, 0, 0);
+            // Lógica para perder o jogo quando colidir com um asteroide
+            Debug.Log("Game Over");
+            painelGameOver.IsActive();
+            Time.timeScale = 0;
+            // Aqui você pode adicionar qualquer outra lógica que desejar, como reiniciar o jogo ou mostrar uma tela de Game Over.
         }
-        if (Input.GetKey ("d") == true)
-        {
-            rb.AddForce(forceMovement * Time.fixedDeltaTime, 0, 0);
-        }
-        
     }
+
 }
