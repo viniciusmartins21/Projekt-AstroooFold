adf3a9bbab72b026fc045567e017a1e1d94ad1d6
vinicius <viniciusmartins.12@hotmail.com>
2023-07-14 11:53:14 -0300

    Fix error repository
    erro de repositorio por conta de arquivo muito grande

--- AStroofold/Assets/Scripts/TESTObjectsPool.cs
+++ AStroofold/Assets/Scripts/TESTObjectsPool.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TEST-ObjectsPool : MonoBehaviour
+public class TESTObjectsPool : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
